3aba9fd198447743a1f5146e88fd39e73fbc0e7b
Yuichi Nishiwaki <yuichi.nishiwaki@gmail.com>
2014-10-17 22:56:27 +0900

    add Top_IO_tb.vhd

--- Top_IO_tb.vhd
+++ Top_IO_tb.vhd
@@ -13,23 +13,64 @@ architecture Behavioral of Top_IO_tb is
       RS_RX : in std_logic);
   end component;
 
-  signal clk_gen : std_logic := '0';
+  signal CLK : std_logic := '0';
   signal rs_tx, rs_rx : std_logic;
+
+  -- global clock period
+  constant CP: time := 15.15 ns;
+  -- bit rate (1 / 9600bps)
+  constant BR: time := 104166 ns;
 
 begin
 
-  myTopIO : Top_IO port map (
-    MCLK1 => clk_gen,
+  myTop : Top_IO port map (
+    MCLK1 => CLK,
     RS_TX => RS_TX,
     RS_RX => RS_RX);
 
   -- clock generator
   process
   begin
-    clk_gen <= '0';
-    wait for 5 ns;
-    clk_gen <= '1';
-    wait for 5 ns;
+    CLK <= '0';
+    wait for CP / 2;
+    CLK <= '1';
+    wait for CP / 2;
+  end process;
+
+  process
+  begin
+
+    RS_RX <= '1';
+
+    wait for (16 * BR);
+
+    wait for BR; RS_RX <= '0'; -- start-bit
+    wait for BR; RS_RX <= '1'; -- data-bit 8'hc5
+    wait for BR; RS_RX <= '0';
+    wait for BR; RS_RX <= '1';
+    wait for BR; RS_RX <= '0';
+    wait for BR; RS_RX <= '0';
+    wait for BR; RS_RX <= '0';
+    wait for BR; RS_RX <= '1';
+    wait for BR; RS_RX <= '1';
+    wait for BR; RS_RX <= '1'; -- stop-bit
+
+    wait for (2 * BR);
+
+    wait for BR; RS_RX <= '0'; -- start-bit
+    wait for BR; RS_RX <= '0'; -- data-bit 8'hf0
+    wait for BR; RS_RX <= '0';
+    wait for BR; RS_RX <= '0';
+    wait for BR; RS_RX <= '0';
+    wait for BR; RS_RX <= '1';
+    wait for BR; RS_RX <= '1';
+    wait for BR; RS_RX <= '1';
+    wait for BR; RS_RX <= '1';
+    wait for BR; RS_RX <= '1'; -- stop-bit
+
+    wait for (16 * BR);
+
+    assert false report "Simulation End." severity failure;
   end process;
 
 end Behavioral;
